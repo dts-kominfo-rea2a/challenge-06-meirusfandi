b53b051717cdbe1854af18efb1a1f89f02aacc3e
Mei Rusfandi <mei.rusfandi@bankmandiri.co.id>
2022-06-12 10:06:14 +0700

    update on fungsi custom js

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,56 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallBack) =>  {
+  let result = [];
+
+  fs.readFile(
+    file1,
+    {
+      encoding : "utf8"
+    },
+    (err, data)=> {
+      if (err) {
+        return fnCallBack(err, null);
+      }
+      
+      const dataMessage = JSON.parse(data);
+      result.push(dataMessage.message.split(" ")[1]);
+    }
+  );
+
+  fs.readFile(
+    file2,
+    {
+      encoding : "utf8"
+    },
+    (err, data)=> {
+      if (err) {
+        return fnCallBack(err, null);
+      }
+      
+      const dataMessage = JSON.parse(data);
+      result.push(dataMessage[0].message.split(" ")[1]);
+    }
+  );
+
+  fs.readFile(
+    file3,
+    {
+      encoding : "utf8"
+    },
+    (err, data)=> {
+      if (err) {
+        return fnCallBack(err, null);
+      }
+      
+      const dataMessage = JSON.parse(data);
+      result.push(dataMessage[0].data.message.split(" ")[1]);
+      fnCallBack(null, result);
+    }
+  );
+
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
